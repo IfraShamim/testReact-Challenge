1deb6f77f7034dc697362e75444c995f428af78a
Ifra Shamim <ifrashamim29@gmail.com>
2025-01-11 02:19:42 +0500

    Done!

--- src/component/Problem4.jsx
+++ src/component/Problem4.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Problem4() {
+  const [isName , isNameValidate] = useState(false); 
+  const [isEmail , isEmailValidate] = useState(false); 
+  const [isPassword , isPasswordValidate] = useState(false); 
+
+  const nameValidate = (name) => {
+    isNameValidate(name.trim().length > 0)
+  }
+  const emailValidate = (email) => {
+    isEmailValidate(email.includes('@'));
+  }
+  const passwordValidate = (password) => {
+    isPasswordValidate(password.length >= 8);
+  }
   return (
-    <div>Problem4</div>
+    <>
+    {/*5. Form with Validation
+Problem:
+Create a form with fields for name, email, and password.
+Validate that:
+The name is not empty.
+The email contains an "@" symbol.
+The password is at least 8 characters long.
+Display error messages if the validation fails.*/}
+<form>
+    <input style={{backgroundColor : isName ? 'green' : 'red'}} type="text" placeholder='Name' onChange={(e) => {
+        nameValidate(e.target.value);
+    }}/><br /><br />
+    <input style={{backgroundColor : isEmail ? 'green' : 'red'}} type="email" placeholder='Email' onChange={(e) => {
+        emailValidate(e.target.value)
+    }}/><br /><br />
+    <input style={{backgroundColor : isPassword ? 'green' : 'red'}} type="password" placeholder='Password' onChange={(e) => {
+        passwordValidate(e.target.value)
+    }}/>
+</form>
+    </>
   )
 }
 
